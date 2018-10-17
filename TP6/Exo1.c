b41acbc01708ffb33d305bd0fb06a70d63627199
Tywacol <tylerdurdenleg@gmail.com>
2018-10-17 19:50:51 +0200

    Create Exo1.c

---
+++ TP6/Exo1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+void mange_et_reecrit(void) 
+{
+  char c;
+  c = getchar();
+  while ( c != EOF ) {
+    putchar(c);
+    c = getchar();
+  }
+}
+
+int main(void)
+{
+  mange_et_reecrit();
+  return 0;
+}
