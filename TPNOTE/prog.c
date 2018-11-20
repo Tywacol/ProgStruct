fa0a44e00878a3f0d9a3ae3945d869a5a03ee377
Tywacol <tylerdurdenleg@gmail.com>
2018-11-20 12:58:32 +0100

    [Format]

--- TPNOTE/prog.c
+++ TPNOTE/prog.c
@@ -51,12 +51,15 @@ void swap(int *x, int *y)
     *y = temp;
 }
 
-// Implementation du tri a bulle
+/*
+* Implementation du tri a bulle
+*/
+
 void bubbleSort(int arr[], int n)
 {
     int i, j;
     for (i = 0; i < n - 1; i++) {
-        // Les i derniers elements sont déjà triees    
+        /* Les i derniers elements sont déjà triees*/    
         for (j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1])
                 swap(&arr[j], &arr[j + 1]);
