4f980753c4472eefd2514c728567f769389c7bce
Tywacol <tylerdurdenleg@gmail.com>
2018-12-08 16:12:11 +0100

    [UPDATE] start of output_grid_periodique

--- Projet_IMA3_2018_2019/main.c
+++ Projet_IMA3_2018_2019/main.c
@@ -53,7 +53,8 @@ void init_grid(char grid[HAUTEUR][LARGEUR], int nb_obstacles, int nb_bonus, int 
         x_tmp = hasard(0, 15);
         y_tmp = hasard(0, 15);
     } while (grid[x_tmp][y_tmp]);
-    grid[x_tmp][y_tmp] = 'T';
+    //grid[x_tmp][y_tmp] = 'T';
+    grid[7][7] = 'T';
 
     /* placement des bonus */
     for (int m = 0; m < nb_bonus; ++m) {
@@ -78,11 +79,12 @@ void init_grid(char grid[HAUTEUR][LARGEUR], int nb_obstacles, int nb_bonus, int 
 }
 
 /* affiche une tableau */
+// VERSION CLASSIQUE
 void output_grid(char grid[HAUTEUR][LARGEUR], int c_pos_x, int c_pos_y)
 {
     int vision = 2 + VISION_BONUS;
 
-    
+
     /* ligne du haut */
     for (int k = 0; k < LARGEUR; ++k) {
         printf(" _");
@@ -94,6 +96,98 @@ void output_grid(char grid[HAUTEUR][LARGEUR], int c_pos_x, int c_pos_y)
         for (int x = 0; x < LARGEUR; ++x) {
             if (y <= c_pos_y + vision && y >= c_pos_y - vision
                 && x <= c_pos_x + vision && x >= c_pos_x - vision) {
+                if (grid[y][x]) {
+                    printf("%c", grid[y][x]);
+                } else if (y == c_pos_y - 1 && x == c_pos_x + 2) {
+                    printf("1");
+                } else if (y == c_pos_y - 2 && x == c_pos_x + 1) {
+                    printf("2");
+                } else if (y == c_pos_y - 2 && x == c_pos_x - 1) {
+                    printf("3");
+                } else if (y == c_pos_y - 1 && x == c_pos_x - 2) {
+                    printf("4");
+                } else if (y == c_pos_y + 1 && x == c_pos_x - 2) {
+                    printf("5");
+                } else if (y == c_pos_y + 2 && x == c_pos_x - 1) {
+                    printf("6");
+                } else if (y == c_pos_y + 2 && x == c_pos_x + 1) {
+                    printf("7");
+                } else if (y == c_pos_y + 1 && x == c_pos_x + 2) {
+                    printf("8");
+                } else {
+                    printf(" ");
+                }
+            } else {
+                printf("%c", 254);
+            }
+            printf(".");
+        }
+        printf("|\n");
+
+    }
+}
+
+// VERSION PERIODIQUE
+void output_grid_periodique(char grid[HAUTEUR][LARGEUR], int c_pos_x, int c_pos_y)
+{
+    int vision = 2 + VISION_BONUS;
+
+
+    /* ligne du haut */
+    for (int k = 0; k < LARGEUR; ++k) {
+        printf(" _");
+    }
+
+
+    int cases_vision[HAUTEUR][HAUTEUR];
+    for (int j = c_pos_y - vision; j < c_pos_y + vision; ++j) {
+        for (int i = c_pos_x - vision; i < c_pos_x + vision; ++i) {
+            if (j < 0) {
+                cases_vision[(HAUTEUR - 1) - j][i] = true;
+            }
+            if (j > (HAUTEUR - 1)) {
+                cases_vision[j - (HAUTEUR - 1)][i] = true;
+            }
+        }
+    }
+
+
+    printf("\n");
+    int depassement_x = 0;
+    int depassement_y = 0;
+    for (int y = 0; y < HAUTEUR; ++y) {
+        printf("|");
+        for (int x = 0; x < LARGEUR; ++x) {
+
+
+            if (is_in(x, y, cases_visions_7_7)) {
+                si
+                la
+            case n'est pas vide : on affiche la case
+                si
+                la
+            case est vide
+                ET
+                qu
+                'il'
+                'y a un deplacemen' : on
+                        affiche
+                le
+                bon numero
+            }
+
+
+            if (y <= c_pos_y + vision && y >= c_pos_y - vision
+                && x <= c_pos_x + vision && x >= c_pos_x - vision) {
+                if (c_pos_x - vision < 0) {
+                    depassement_x = abs(c_pos_x - vision);
+                    for (int i = 0; i < depassement_x; ++i) {
+                        coord_tmp[1][0]
+                        " '1.x' = L-abs"]
+                        // coord_tmp [pos][x|y]
+                        // pos = 1..8, x|y = 0|1
+                    }
+                }
                 if (grid[y][x]) {
                     printf("%c", grid[y][x]);
                 } else if (y == c_pos_y - 1 && x == c_pos_x + 2) {
@@ -139,7 +233,7 @@ void choixToXY(int c_pos_x, int c_pos_y, int f_pos, int *futur_x, int *futur_y)
         break;
     case 3:
         *futur_y = c_pos_y - 2;
-        *futur_x = c_pos_x + 1;
+        *futur_x = c_pos_x - 1;
         break;
     case 4:
         *futur_y = c_pos_y - 1;
@@ -151,7 +245,7 @@ void choixToXY(int c_pos_x, int c_pos_y, int f_pos, int *futur_x, int *futur_y)
         break;
     case 6:
         *futur_y = c_pos_y + 2;
-        *futur_x = c_pos_x + -1;
+        *futur_x = c_pos_x - 1;
         break;
     case 7:
         *futur_y = c_pos_y + 2;
@@ -171,10 +265,7 @@ void move(char grid[LARGEUR][HAUTEUR], int *c_pos_x, int *c_pos_y, int f_pos)
 {
     int futur_x;
     int futur_y;
-    printf("avant conversion. c_pos_x = %d, c_pos_y = %d\n", *c_pos_x, *c_pos_y);
     choixToXY(*c_pos_x, *c_pos_y, f_pos, &futur_x, &futur_y);
-    printf("apres conversion. c_pos_x = %d, c_pos_y = %d,\n futur_x = %d,"
-           " futur_y = %d\n", *c_pos_x, *c_pos_y, futur_x, futur_y);
     switch (grid[futur_y][futur_x]) {
     case 'T':
         WON = true;
@@ -220,7 +311,7 @@ int main()
     int chevalier_pos_y = hasard(5, 10);
 
 
-    int futur_pos = 1;
+    int futur_pos = 0;
     char grid[HAUTEUR][LARGEUR];
     init_grid(grid, NB_OBSTACLES, NB_BONUS, chevalier_pos_x, chevalier_pos_y);
 
@@ -231,15 +322,22 @@ int main()
             printf("*                                                *\n");
             printf("*             BRAVO ! Vous avez gagne en         *\n");
             printf("*                                                *\n");
-            printf("*                 %d iterations !                *\n", ITER);
+            printf("*                 %d iterations !                 *\n", ITER);
             printf("*                                                *\n");
             printf("************** 'R' pour recommencer **************\n");
-            WON = false;
+            scanf("%d", &futur_pos);
+            if (futur_pos == 'R') {
+                Restart = true;
+            } else {
+                Quit = true;
+            }
+
         }
         if (Restart) {
             chevalier_pos_x = hasard(5, 10);
             chevalier_pos_y = hasard(5, 10);
             init_grid(grid, NB_OBSTACLES, NB_BONUS, chevalier_pos_x, chevalier_pos_y);
+            Restart = false;
         }
         if (Quit) {
             printf("Fin du programme..\n");
@@ -249,12 +347,9 @@ int main()
         printf("\n");
         printf("Prochaine position (R pour recommencer) : ");
         scanf("%d", &futur_pos);
-        Restart = (futur_pos == 'R');
-
         move(grid, &chevalier_pos_x, &chevalier_pos_y, futur_pos);
         //system("clear");
-    } while (!WON && !Quit && !Restart);
+    } while (!Quit);
 
     return 0;
 }
-
